2a2c0d18a794351f161590f191c0a40f951cabfd
Shada11haddad <Shada.hadad123@gmail.com>
2026-07-09 11:30:28 +0300

    challeng 4 : Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(){
-	printf("Programming is like building a multilingual puzzle, followed by a new line.");
-	return 0;
-}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+	}
