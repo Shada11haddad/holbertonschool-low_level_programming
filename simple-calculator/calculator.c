47a22b7710e3338fbf47fe304f9ea89755897412
Shada11haddad <Shada.hadad123@gmail.com>
2026-07-15 17:35:20 +0300

    calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -4,14 +4,9 @@
 int main()
 {
 
-        //Intialize valuble
 
-        int x = 0;
-        int y = 0;
-        int ans = 0;
         bool start = true;
         int opt = 0;
-        //while loob
         while (start == true){
         printf("Please select :\n 1) Add\n 2)Subtract\n 3)Mulitply\n 4)Divide\n 0) Quit\n ");
         scanf("%d" , &opt);
