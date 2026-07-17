896f456916e69d4d4e5049dc6ef76f2efb7dd6f3
Shada11haddad <Shada.hadad123@gmail.com>
2026-07-18 00:37:40 +0300

    poiter3

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,8 +6,8 @@
  * @s: the string to search
  * @c: the character to find
  *
- * Return: a pointer to the first occurrence of the character c in the string s,
- * or NULL if the character is not found
+ * Return: a pointer to the first occurrence of the character c in the
+ * string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
