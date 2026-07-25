08722b11098e183eaab5fd240aca52ed48584c8e
Shada11haddad <Shada.hadad123@gmail.com>
2026-07-25 07:46:54 +0300

    Trust no on

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,8 +3,8 @@
 
 /**
  * array_range - creates an array of integers from min to max
- * @min: starting value (included)
- * @max: maximum value (included)
+ * @min: starting value
+ * @max: maximum value
  *
  * Return: pointer to the newly created array, or NULL on failure
  */
@@ -13,19 +13,15 @@ int *array_range(int min, int max)
 	int *arr;
 	int i, size;
 
-	/* 1. الشرط الدفاعي: إذا كان الحد الأدنى أكبر من الأقصى */
 	if (min > max)
 		return (NULL);
 
-	/* 2. حساب حجم المصفوفة الإجمالي */
 	size = (max - min) + 1;
 
-	/* 3. حجز الذاكرة */
 	arr = malloc(sizeof(int) * size);
 	if (arr == NULL)
 		return (NULL);
 
-	/* 4. تعبئة المصفوفة بالأرقام بالتسلسل */
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = min;
