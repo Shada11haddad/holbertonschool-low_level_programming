29eb28cba88fead5f319ce11be4a699dcb4dd3f6
Shada11haddad <Shada.hadad123@gmail.com>
2026-08-01 05:52:11 +0300

    function poinete

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: the array of integers to search through
+ * @size: the number of elements in the array
+ * @cmp: a pointer to the function used to compare values
+ *
+ * Return: the index of the first element for which cmp does not return 0.
+ * If no element matches or if size <= 0, returns -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
+}
---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed as argument to the program
+ *
+ * Return: a pointer to the function that corresponds to the operator,
+ * or NULL if s does not match any expected operator.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		/* Match exact character and ensure the string is only 1 char long */
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
