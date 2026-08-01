94477f73bd40e4eaa7a0ffcf542888eba52678e0
Shada11haddad <Shada.hadad123@gmail.com>
2026-08-01 05:53:23 +0300

    function poinete

---
+++ function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple mathematical operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0on success, exits with 98, 99, or 100 on error.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	func = get_op_func(argv[2]);
+
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", func(num1, num2));
+
+	return (0);
+}
